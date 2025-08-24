2b9295d79817ff7c5456302366b9729aa4327d9f
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-08-24 15:23:02 +0530

    refactor: added optimal  approach to sort arrays of 0 1 2

--- src/com/dsa/problems/arrays/SortArrayOf012.java
+++ src/com/dsa/problems/arrays/SortArrayOf012.java
@@ -6,7 +6,8 @@ public class SortArrayOf012 {
     public static void main(String[] args) {
         int[] arr = {2,0,2,1,1,0};
         //sort(arr);
-        manualCount(arr);
+        //manualCount(arr);
+        DNF(arr);
 
     }
     //T : O(n log n)
@@ -48,7 +49,36 @@ public class SortArrayOf012 {
             System.out.println(Arrays.toString(arr));
         }
 
+        //Optimal : Dutch National Flag Algorithm
+        //T : O(n) -> One pass
+        //S : O(1)
 
+    static void DNF(int[] arr){
+        int left = 0;
+        int mid = 0;
+        int right = arr.length-1;
+
+        while(mid<=right){
+            if(arr[mid]==0){
+                int temp = arr[left];
+                arr[left] = arr[mid];
+                arr[mid] = temp;
+                left++;
+                mid++;
+
+            }else if (arr[mid]==1){
+                mid++;
+            }else{
+                int temp = arr[mid];
+                arr[mid] = arr[right];
+                arr[right]= temp;
+                mid++;
+                right--;
+            }
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 
 
 }
