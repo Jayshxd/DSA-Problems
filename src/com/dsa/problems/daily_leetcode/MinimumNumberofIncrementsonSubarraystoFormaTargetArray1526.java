e4e050ea6a04213c30f5b05d7b9d2e51ec81354d
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-10-30 09:23:23 +0530

    feat : Daily leetcode oct 30

---
+++ src/com/dsa/problems/daily_leetcode/MinimumNumberofIncrementsonSubarraystoFormaTargetArray1526.java
@@ -0,0 +1,13 @@
+package com.dsa.problems.daily_leetcode;
+
+public class MinimumNumberofIncrementsonSubarraystoFormaTargetArray1526 {
+    class Solution {
+        public int minNumberOperations(int[] target) {
+            int s = target[0];
+            for (int i = 1; i < target.length; i++)
+                if (target[i] > target[i - 1])
+                    s += (target[i] - target[i - 1]);
+            return s;
+        }
+    }
+}
