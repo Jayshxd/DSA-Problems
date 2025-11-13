bfc5602db4ecc5e8c976a06d52e08aa8016665c8
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-11-13 23:49:47 +0530

    feat : Odd Even LinkedList

---
+++ src/com/dsa/problems/linked_list/OddEvenLinkedList.java
@@ -0,0 +1,30 @@
+package com.dsa.problems.linked_list;
+
+public class OddEvenLinkedList {
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+
+    class Solution {
+        public ListNode oddEvenList(ListNode head) {
+            if(head==null || head.next==null) return head;
+            ListNode odd = head;
+            ListNode even = head.next;
+            ListNode evenHead = even;
+            while((odd!=null && odd.next!=null)||(even!=null && even.next!=null)){
+                odd.next = even.next;
+                if(odd.next!=null){
+                    odd = odd.next;
+                }
+                even.next = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+            return head;
+        }
+    }
+}
