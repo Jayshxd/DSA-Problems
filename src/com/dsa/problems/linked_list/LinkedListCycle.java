ef9f7545ca3f6ddee499fcbf939690fb46d499ce
Jayesh Hiwarkar <jayeshhiwarkar75@gmail.com>
2025-11-18 21:24:13 +0530

    feat : Linked List Cycle

---
+++ src/com/dsa/problems/linked_list/LinkedListCycle.java
@@ -0,0 +1,27 @@
+package com.dsa.problems.linked_list;
+
+public class LinkedListCycle {
+
+
+      class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) {
+              val = x;
+              next = null;
+          }
+      }
+
+    public class Solution {
+        public boolean hasCycle(ListNode head) {
+            if(head==null || head.next==null) return false;
+            ListNode slow = head , fast = head;
+            while(fast!=null && fast.next!=null){
+                slow=slow.next;
+                fast=fast.next.next;
+                if(slow==fast) return true;
+            }
+            return false;
+        }
+    }
+}
